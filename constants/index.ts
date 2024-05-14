e27ba528fd10419efe391249d754c6d8405d1afc
ibrarmunircoder <ibrarmunir009@gmail.com>
2024-05-14 23:37:04 +0500

    worked on navbar

---
+++ constants/index.ts
@@ -0,0 +1,136 @@
+import { SidebarLink } from '@/types';
+
+export const themes = [
+  { value: 'light', label: 'Light', icon: '/assets/icons/sun.svg' },
+  { value: 'dark', label: 'Dark', icon: '/assets/icons/moon.svg' },
+  { value: 'system', label: 'System', icon: '/assets/icons/computer.svg' },
+];
+
+type TopQuestion = {
+  _id: number;
+  title: string;
+};
+
+export const topQuestions: TopQuestion[] = [
+  {
+    title:
+      'Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?',
+    _id: 2,
+  },
+  {
+    _id: 2,
+    title: 'Is it only me or the font is bolder than necessary?',
+  },
+  {
+    _id: 2,
+    title: 'Can I get the course for free?',
+  },
+  {
+    _id: 2,
+    title: 'Redux Toolkit Not Updating State as Expected',
+  },
+  {
+    _id: 2,
+    title: 'Async/Await Function Not Handling Errors Properly',
+  },
+];
+
+type TopTag = {
+  _id: string;
+  name: string;
+  totalQuestions: number;
+};
+
+export const topTags: TopTag[] = [
+  {
+    _id: '1',
+    name: 'Nextjs',
+    totalQuestions: 5,
+  },
+  {
+    _id: '0',
+    name: 'Test',
+    totalQuestions: 5,
+  },
+  {
+    _id: '4',
+    name: 'React',
+    totalQuestions: 5,
+  },
+  {
+    _id: '7',
+    name: 'Javascript',
+    totalQuestions: 5,
+  },
+  {
+    _id: '2',
+    name: 'Git',
+    totalQuestions: 5,
+  },
+];
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    imgURL: '/assets/icons/home.svg',
+    route: '/',
+    label: 'Home',
+  },
+  {
+    imgURL: '/assets/icons/users.svg',
+    route: '/community',
+    label: 'Community',
+  },
+  {
+    imgURL: '/assets/icons/star.svg',
+    route: '/collection',
+    label: 'Collections',
+  },
+  {
+    imgURL: '/assets/icons/suitcase.svg',
+    route: '/jobs',
+    label: 'Find Jobs',
+  },
+  {
+    imgURL: '/assets/icons/tag.svg',
+    route: '/tags',
+    label: 'Tags',
+  },
+  {
+    imgURL: '/assets/icons/user.svg',
+    route: '/profile',
+    label: 'Profile',
+  },
+  {
+    imgURL: '/assets/icons/question.svg',
+    route: '/ask-question',
+    label: 'Ask a question',
+  },
+];
+
+export const BADGE_CRITERIA = {
+  QUESTION_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  QUESTION_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  TOTAL_VIEWS: {
+    BRONZE: 1000,
+    SILVER: 10000,
+    GOLD: 100000,
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     container: {
@@ -37,41 +38,40 @@ const config: Config = {
         },
         'accent-blue': '#1DA1F2',
       },
-    },
-    fontFamily: {
-      inter: ['var(--font-inter)'],
-      spaceGrotesk: ['var(--font-spaceGrotesk)'],
-    },
-    boxShadow: {
-      'light-100':
-        '0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px',
-      'light-200': '10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
-      'light-300': '-10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
-      'dark-100': '0px 2px 10px 0px rgba(46, 52, 56, 0.10)',
-      'dark-200': '2px 0px 20px 0px rgba(39, 36, 36, 0.04)',
-    },
-    backgroundImage: {
-      'auth-dark': "url('/assets/images/auth-dark.png')",
-      'auth-light': "url('/assets/images/auth-light.png')",
-    },
-    screens: {
-      xs: '420px',
-    },
-    keyframes: {
-      'accordion-down': {
-        from: { height: '0' },
-        to: { height: 'var(--radix-accordion-content-height)' },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        spaceGrotesk: ['var(--font-spaceGrotesk)'],
       },
-      'accordion-up': {
-        from: { height: 'var(--radix-accordion-content-height)' },
-        to: { height: '0' },
+      boxShadow: {
+        'light-100':
+          '0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)',
+        'light-200': '10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
+        'light-300': '-10px 10px 20px 0px rgba(218, 213, 213, 0.10)',
+        'dark-100': '0px 2px 10px 0px rgba(46, 52, 56, 0.10)',
+        'dark-200': '2px 0px 20px 0px rgba(39, 36, 36, 0.04)',
       },
-    },
-    animation: {
-      'accordion-down': 'accordion-down 0.2s ease-out',
-      'accordion-up': 'accordion-up 0.2s ease-out',
+      backgroundImage: {
+        'auth-dark': "url('/assets/images/auth-dark.png')",
+        'auth-light': "url('/assets/images/auth-light.png')",
+      },
+      screens: {
+        xs: '420px',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
-export default config;
